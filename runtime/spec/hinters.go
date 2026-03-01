e8f20f8bc9b88698ed4b2694ae633b817f2ff444
soonkuk <soonkuk@gmail.com>
2026-03-01 14:29:04 +0900

    refact: split runtime packages and restore ccmds compatibility

--- runtime/spec/hinters.go
+++ runtime/spec/hinters.go
@@ -1,16 +1,11 @@
-package cmds
+package spec
 
 import (
-	ccmds "github.com/imfact-labs/currency-model/app/cmds"
 	"github.com/imfact-labs/mitum2/util/encoder"
 	"github.com/imfact-labs/payment-model/operation/payment"
 	"github.com/imfact-labs/payment-model/state"
 	"github.com/imfact-labs/payment-model/types"
-	"github.com/pkg/errors"
 )
-
-var Hinters []encoder.DecodeDetail
-var SupportedProposalOperationFactHinters []encoder.DecodeDetail
 
 var AddedHinters = []encoder.DecodeDetail{
 	// revive:disable-next-line:line-length-limit
@@ -35,28 +30,4 @@ var AddedSupportedHinters = []encoder.DecodeDetail{
 	{Hint: payment.TransferFactHint, Instance: payment.TransferFact{}},
 	{Hint: payment.UpdateAccountSettingFactHint, Instance: payment.UpdateAccountSettingFact{}},
 	{Hint: payment.WithdrawFactHint, Instance: payment.WithdrawFact{}},
-}
-
-func init() {
-	Hinters = append(Hinters, ccmds.Hinters...)
-	Hinters = append(Hinters, AddedHinters...)
-
-	SupportedProposalOperationFactHinters = append(SupportedProposalOperationFactHinters, ccmds.SupportedProposalOperationFactHinters...)
-	SupportedProposalOperationFactHinters = append(SupportedProposalOperationFactHinters, AddedSupportedHinters...)
-}
-
-func LoadHinters(encs *encoder.Encoders) error {
-	for i := range Hinters {
-		if err := encs.AddDetail(Hinters[i]); err != nil {
-			return errors.Wrap(err, "add hinter to encoder")
-		}
-	}
-
-	for i := range SupportedProposalOperationFactHinters {
-		if err := encs.AddDetail(SupportedProposalOperationFactHinters[i]); err != nil {
-			return errors.Wrap(err, "add supported proposal operation fact hinter to encoder")
-		}
-	}
-
-	return nil
 }
